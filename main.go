9ab37b787ae5e3043e3df176da9c22790303f7ea
wioenena-q <wioenena@gmail.com>
2023-06-26 23:03:25 +0300

    chore: i backed up a few of my files!

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"github.com/wioenena-q/wackup/config"
+	"github.com/wioenena-q/wackup/utils"
+)
+
 func main() {
-	println("Hello World")
+	json_contens := utils.ReadFileBytes("wackup.json")
+	config := config.NewWackupConfig(json_contens)
+	config.Load()
 }
